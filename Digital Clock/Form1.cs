6f88a52d69b5019e8834a7a2fcd2da9e5524db4c
Ryan Kuchenthal <koochenthal0@gmail.com>
2023-01-09 14:45:23 -0800

    added logic for time, then assigned it to the label text

--- Digital Clock/Form1.cs
+++ Digital Clock/Form1.cs
@@ -12,6 +12,9 @@ namespace Digital_Clock
 {
     public partial class clock : Form
     {
+        
+        Timer t = new Timer();
+        
         public clock()
         {
             InitializeComponent();
@@ -19,6 +22,55 @@ namespace Digital_Clock
 
         private void label1_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void clock_Load(object sender, EventArgs e)
+        {
+            t.Interval= 1000;
+            t.Tick += new EventHandler(this.t_Tick);
+            t.Start();
+        }
+
+        private void t_Tick(object sender, EventArgs e)
+        {
+            int hh = DateTime.Now.Hour;
+            int mm = DateTime.Now.Minute; 
+            int ss = DateTime.Now.Second;
+
+            string time = "";
+
+            //hour 
+            if (hh < 10)
+            {
+                time += "0" + hh;
+            }
+            else
+            {
+                time += hh;
+            }
+            time += ":"; //colon between hh:mm
+            //minutes
+            if (mm < 10)
+            {
+                time += "0" + mm;
+            }
+            else
+            {
+                time += mm;
+            }
+            time += ":"; //colon between mm:ss
+            //seconds
+            if (ss < 10)
+            {
+                time += "0" + ss;
+            }
+            else
+            {
+                time += ss;
+            }
+
+            label1.Text = time;
 
         }
     }
