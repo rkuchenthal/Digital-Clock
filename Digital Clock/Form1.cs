40c8d6618af59f2174a430676862a63876f65306
Ryan Kuchenthal <koochenthal0@gmail.com>
2023-01-09 14:26:19 -0800

    Add project files.

---
+++ Digital Clock/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Digital_Clock
+{
+    public partial class clock : Form
+    {
+        public clock()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
